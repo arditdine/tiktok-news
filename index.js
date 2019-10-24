91af6c298155e662b8a1dc59b7840d87edc14346
ardit dine <ardit.dine@iqair.com>
2019-10-24 16:32:05 +0800

    structure organized

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict'
+const get = require('lodash.get')
 
 require('dotenv').config()
 
-const _ = require("lodash")
-const request = require("superagent")
-const nodemailer = require("nodemailer")
-
-const sqlite = require("./db")
+const { sendMail, getResults, generateHTML } = require('./lib/util')
+const sqlite = require("./lib/db")
 
 const URLS = [
   "https://newsroom.tiktok.com/web/api/v2/project/articleList/?project_key=newsroom&category_key=en-us&limit=20&page=0&tag_names=news",
@@ -15,7 +13,7 @@ const URLS = [
 ]
 
 const BASE_URL = "https://newsroom.tiktok.com"
-const TO_EMAIL = "yingzhi.yang@thomsonreuters.com"
+const TO_EMAIL = "ardit_dine@hotmail.com"
 
 
 ;(async () => {
@@ -34,36 +32,36 @@ const TO_EMAIL = "yingzhi.yang@thomsonreuters.com"
   const news = []
   const bulk = []
 
+  // Iterrate over US news and push only new articles
   for (let n of us) {
     const article = await sqlite.get(`SELECT ID FROM articles WHERE ARTICLE_KEY='${n.article_key}'`)
     if (!article) {
       bulk.push(`("${n.article_key}", "${n.publish_time}", "${n.update_time}")`)
-      n.article_url = `${BASE_URL}/en-us/${n.article_key}`
-      news.push(n)
+      news.push(etlTikTok(n, 'en-us'))
     }
   }
 
+  // Iterrate over GB news and push only new articles
   for (let n of gb) {
     const article = await sqlite.get(`SELECT ID FROM articles WHERE ARTICLE_KEY='${n.article_key}'`)
     if (!article) {
       bulk.push(`("${n.article_key}", "${n.publish_time}", "${n.update_time}")`)
-      n.article_url = `${BASE_URL}/en-gb/${n.article_key}`
-      news.push(n)
+      news.push(etlTikTok(n, 'en-gb'))
     }
   }
 
+  // Iterrate over Indian news and push only new articles
   for (let n of india) {
     const article = await sqlite.get(`SELECT ID FROM articles WHERE ARTICLE_KEY='${n.article_key}'`)
     if (!article) {
       bulk.push(`("${n.article_key}", "${n.publish_time}", "${n.update_time}")`)
-      n.article_url = `${BASE_URL}/en-in/${n.article_key}`
-      news.push(n)
+      news.push(etlTikTok(n, 'en-in'))
     }
   }
   
   if (news.length){
     try{
-      await sendMail({ to: TO_EMAIL, subject: "Tik Tok Website Updated", html: generateHTML(news) })
+      await sendMail({ to: TO_EMAIL, subject: "TikTok Website Updated", html: generateHTML(news) })
 
       if(bulk.length != news.length) console.log(`OOOOO, bulk ${bulk.length} - ${news.length} news!!`)
 
@@ -72,48 +70,21 @@ const TO_EMAIL = "yingzhi.yang@thomsonreuters.com"
     } catch(ex){
       console.error(ex)
     }
+
+    console.log(`Finished: ${news.length} news`, new Date().toISOString())
+  }else{
+    console.log("Finished", new Date().toISOString())
   }
 
-  console.log("Finished")
+ 
 })()
 
-async function sendMail({ to, subject, html }) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD
-    }
-  });
-
-  return await transporter.sendMail({
-    from: process.env.SMTP_USERNAME,
-    to,
-    subject,
-    html
-  });
-}
-
-async function getResults(url) {
-  try {
-    const res = await request(url);
-    return _.get(res.body, ["body", "article_list"], [])
-  } catch (ex) {
-    console.log(ex)
-    return []
+function etlTikTok(news, lang) {
+  return {
+    image: news.article_image ? 'https:' + news.article_image : null,
+    title: get(news, 'article_title'),
+    url: `${BASE_URL}/${lang}/${news.article_key}`,
+    desc: get(news, 'article_desc'),
+    date: get(news, 'publish_time')
   }
-}
-
-function generateHTML(news) {
-  let template = `<style type="text/css"> body, html, .body{background: #f3f3f3 !important;}.container.header{background: #f3f3f3;}.body-border{border-top: 8px solid #663399;}</style> <spacer size="16"></spacer> <container class="header"> <row> <columns> <h1 class="text-center">New News on TikTok Website</h1> </columns> </row> </container> <container class="body-border"> <row>`
-
-  for (let n of news) {
-    const image = !_.isEmpty(n.article_image)
-      ? `<img src="https:${n.article_image}">`
-      : null;
-    template += `<columns> <spacer size="32"></spacer> <center>${image}</center> <spacer size="16"></spacer> <h4><a href="${n.article_url}">${n.article_title}</a></h4> <p>${n.article_desc}</p><p style="color: #589ce8;">Published: ${n.publish_time}</p></columns><hr>`
-  }
-  template += `</row> <spacer size="16"></spacer> </container>`
-
-  return template
 }
